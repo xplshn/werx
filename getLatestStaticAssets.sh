933ec00210109cc2eec351ab389f53d4b18095b6
xplshn <xplshn@murena.io>
2025-05-22 04:58:44 -0300

    update deps
    Signed-off-by: xplshn <xplshn@murena.io>

--- getLatestStaticAssets.sh
+++ getLatestStaticAssets.sh
@@ -1,4 +1,6 @@
-#!/bin/sh -x
+#!/bin/sh
+
+set -ex
 
 CDN_MERMAID="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"
 CDN_SERENITYOS_EMOJI_FONT_CSS="https://linusg.github.io/serenityos-emoji-font/SerenityOS-Emoji.css"
@@ -8,31 +10,57 @@ CDN_HIGHLIGHTJS_BASH="https://unpkg.com/@highlightjs/cdn-assets/languages/bash.m
 CDN_HIGHLIGHTJS_GO="https://unpkg.com/@highlightjs/cdn-assets/languages/go.min.js"
 CDN_HIGHLIGHTJS_C="https://unpkg.com/@highlightjs/cdn-assets/languages/c.min.js"
 CDN_MATHJAX="https://www.unpkg.com/mathjax/es5/tex-mml-svg.js"
+CDN_CHARTJS="https://cdn.jsdelivr.net/npm/chart.js"
+CDN_D3JS="https://cdn.jsdelivr.net/npm/d3@7.9.0/dist/d3.min.js"
+CDN_TOPOJS="https://unpkg.com/topojson@3.0.2/dist/topojson.min.js"
+CDN_TOPOJS_CLIENT="https://unpkg.com/topojson-client@3.0.1/dist/topojson-client.min.js"
 
 fetch() {
-    url="$1"
-    output="$2"
+    mode="$1"     # empty | --minimize | --minimize-and-bundle
+    url="$2"
+    out="$3"
+    tmp="${out}.tmp.${out##*.}"
 
-    if command -v curl > /dev/null 2>&1; then
-        curl -Lso "$output" "$url"
-    elif command -v wget > /dev/null 2>&1; then
-        wget -qO "$output" "$url"
+    if _CMD="$(command -v curl)"; then
+        "$_CMD" -Lso "$tmp" "$url"
+    elif _CMD="$(command -v wget)"; then
+        "$_CMD" -qO "$tmp" "$url"
     else
-        echo "Neither curl nor wget is available. Please acquire one of them via dbin"
+        echo "ERROR: curl or wget required" >&2
         exit 1
     fi
+
+    case "$mode" in
+        --minimize|--minimize-and-bundle)
+            if [ "${out##*.}" = js ] && _ESBUILD="$(command -v esbuild)"; then
+                if [ "$mode" = "--minimize-and-bundle" ]; then
+                    "$_ESBUILD" "$tmp" --bundle --minify --outfile="$out"
+                else
+                    "$_ESBUILD" "$tmp" --minify --outfile="$out"
+                fi
+                rm -f "$tmp"
+                return
+            fi
+            ;;
+    esac
+
+    mv "$tmp" "$out"
 }
 
-if [ ! -d ./static/js ]; then
-    echo "Where are we?"
-    echo "Run this script in the WERX theme folder"
-fi
+[ -d ./static/js ] || {
+    echo "ERROR: Run this script in the WERX theme folder"
+    exit 1
+}
 
-fetch "$CDN_SERENITYOS_EMOJI_FONT_CSS" ./static/css/SerenityOS-Emoji.css
-fetch "$CDN_MERMAID" ./static/js/mermaid.min.js
-fetch "$CDN_HIGHLIGHTJS" ./static/js/highlight.min.js
-fetch "$CDN_HIGHLIGHTJS_BASIC" ./static/js/highlight-basic.min.js
-fetch "$CDN_HIGHLIGHTJS_BASH" ./static/js/highlight-bash.min.js
-fetch "$CDN_HIGHLIGHTJS_GO" ./static/js/highlight-go.min.js
-fetch "$CDN_HIGHLIGHTJS_C" ./static/js/highlight-c.min.js
-fetch "$CDN_MATHJAX" ./static/js/tex-mml-svg.js
+fetch "" "$CDN_SERENITYOS_EMOJI_FONT_CSS" ./static/css/SerenityOS-Emoji.css
+fetch --minimize-and-bundle "$CDN_MERMAID" ./static/js/mermaid.min.js
+fetch "" "$CDN_HIGHLIGHTJS" ./static/js/highlight.min.js
+fetch "" "$CDN_HIGHLIGHTJS_BASIC" ./static/js/highlight-basic.min.js
+fetch "" "$CDN_HIGHLIGHTJS_BASH" ./static/js/highlight-bash.min.js
+fetch "" "$CDN_HIGHLIGHTJS_GO" ./static/js/highlight-go.min.js
+fetch "" "$CDN_HIGHLIGHTJS_C" ./static/js/highlight-c.min.js
+fetch --minimize-and-bundle "$CDN_MATHJAX" ./static/js/tex-mml-svg.js
+fetch --minimize "$CDN_CHARTJS" ./static/js/chart.min.js
+fetch "" "$CDN_D3JS" ./static/js/d3.min.js
+fetch "" "$CDN_TOPOJS" ./static/js/topojson.min.js
+fetch "" "$CDN_TOPOJS_CLIENT" ./static/js/topojson-client.min.js
